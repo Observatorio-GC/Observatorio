3a249fa6771e7ab6e8346501df540a819710b3a8
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2020-06-17 16:51:36 -0300

    Add files via upload

---
+++ scripts/Albergues Transitorios.js
@@ -0,0 +1,13 @@
+var Albergue transitorio={
+"type": "FeatureCollection",
+"name": "Albergues Transitorios",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Mitchens Love", "Ubicacion": "Primitivo de la Reta 430", "Camas": 26 }, "geometry": { "type": "Point", "coordinates": [ -68.830327496807598, -32.931597106946242 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Monumental", "Ubicacion": "Santiago del Estero 1972", "Camas": 26 }, "geometry": { "type": "Point", "coordinates": [ -68.866512370363836, -32.901653789694357 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "Flamingo", "Ubicacion": "Terrada 325", "Camas": 30 }, "geometry": { "type": "Point", "coordinates": [ -68.817503380297111, -32.931028181721935 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Nombre": "One More", "Ubicacion": "Terrada 504", "Camas": 23 }, "geometry": { "type": "Point", "coordinates": [ -68.818489991340797, -32.932739546817004 ] } },
+{ "type": "Feature", "properties": { "id": 5, "Nombre": "Hostal Do Sol", "Ubicacion": "Paso de los Andes 1224", "Camas": 12 }, "geometry": { "type": "Point", "coordinates": [ -68.8598975087274, -32.913444901913572 ] } },
+{ "type": "Feature", "properties": { "id": 6, "Nombre": "Chalet de Bassi", "Ubicacion": "Cervantes 1795", "Camas": 15 }, "geometry": { "type": "Point", "coordinates": [ -68.843351676113258, -32.947551592105093 ] } }
+]
+}
---
+++ scripts/JardinesMunicipales.js
@@ -0,0 +1,16 @@
+var JardinesMunicipales ={
+"type": "FeatureCollection",
+"name": "Jardines Municipales",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "numero": "4", "nivel": "Jardin Maternal", "nombre": "Chispitas de Esperanza", "domicilio": "Pte Illia 981", "email": null, "telefono": "0261-156574886", "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.870981668608593, -32.939269324287324 ] ] } },
+{ "type": "Feature", "properties": { "numero": "5", "nivel": "Jardin Maternal", "nombre": "Estrellitas", "domicilio": "Mariano Moreno S\/N", "email": null, "telefono": "0261-155753253", "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.838769394116213, -32.958119151304167 ] ] } },
+{ "type": "Feature", "properties": { "numero": "1", "nivel": "Jardin Maternal", "nombre": "Ternuritas", "domicilio": "Renato Della Santa 650", "email": null, "telefono": "0261-4282810", "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.861333802118395, -32.918854132562714 ] ] } },
+{ "type": "Feature", "properties": { "numero": "8", "nivel": "Jardin Maternal", "nombre": "Mi Angelito", "domicilio": "Volcan Sta Maria 1400", "email": null, "telefono": null, "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.828689790909948, -32.944558980239748 ] ] } },
+{ "type": "Feature", "properties": { "numero": "7", "nivel": "Jardin Maternal", "nombre": "Patitas Sueltas", "domicilio": "Lautaro 2284", "email": null, "telefono": "4288464", "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.869742978150597, -32.905947983133807 ] ] } },
+{ "type": "Feature", "properties": { "numero": "6", "nivel": "Jardin Maternal", "nombre": "Gotitas de Miel", "domicilio": "Bustamante y Bahia Ushuaia", "email": null, "telefono": null, "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.83124970335048, -32.9573016886755 ] ] } },
+{ "type": "Feature", "properties": { "numero": "2", "nivel": "Jardin Maternal", "nombre": "El Monito", "domicilio": "Colon 966", "email": null, "telefono": "261 4350502", "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.848110620625263, -32.93552365933521 ] ] } },
+{ "type": "Feature", "properties": { "numero": "3", "nivel": "Jardin Maternal", "nombre": "Picardias", "domicilio": "Manzana \"12\"", "email": null, "telefono": "0261-4397173", "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.869519010619129, -32.94742511066918 ] ] } },
+{ "type": "Feature", "properties": { "numero": "9", "nivel": "Jardin Maternal", "nombre": "Multicolores y Ludoteca", "domicilio": "Salvador Arias y Pinamar", "email": null, "telefono": null, "modalidad": "Estatal" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.874610772609458, -32.932762332990443 ] ] } }
+]
+}
