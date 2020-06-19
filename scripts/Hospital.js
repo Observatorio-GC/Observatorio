90fab549c70002874b13a9bfe50544ed3bf5538d
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2020-06-19 13:53:33 -0300

    Delete Hospital.js

--- scripts/Hospital.js
+++
@@ -1,8 +0,0 @@
-var Hospital ={
-"type": "FeatureCollection",
-"name": "Hospital",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "qc_id": 0, "nombre": "Lencinas", "dpto": "Godoy Cruz", "distrito": "V.Hipodromo", "direccion": "Talcahuano S\/N", "telefono": "4272600 \/ 4272773", "complejida": "II MONOVALENTE", "n_camas": "132", "jerraquia": "\/VI\/", "especialid": "ENF.END\/EP", "servicio": "INTER. Y CONS. EX", "radio_inf": "24000", "clase": "Hospital", "responsabl": "Lic. Luis Ten Huver" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.870817296300288, -32.903574896312243 ] ] } }
-]
-}
