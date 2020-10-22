1fdba2de0abd78fc0ab78307f2334e7dc63f6d96
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2020-10-22 10:03:17 -0300

    Add files via upload

---
+++ scripts/Solmaforos.js
@@ -0,0 +1,22 @@
+var Solmaforos ={
+"type": "FeatureCollection",
+"name": "Solmaforos",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Ubicacion": "Huergo y Guemes", "Nombre": "Club Petroleros" }, "geometry": { "type": "Point", "coordinates": [ -68.853524954642467, -32.928039256248347 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Ubicacion": "Montes de Oca y Armani", "Nombre": "Club Amigos del Jockey" }, "geometry": { "type": "Point", "coordinates": [ -68.868988717047685, -32.915045035092191 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Ubicacion": "9 de Julio y Alsina", "Nombre": "Club Impsa" }, "geometry": { "type": "Point", "coordinates": [ -68.812350600339954, -32.935835715827352 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Ubicacion": "9 de Julio y Sarmiento", "Nombre": "Acuatica Salud y Natacion" }, "geometry": { "type": "Point", "coordinates": [ -68.816204301986517, -32.944320432868878 ] } },
+{ "type": "Feature", "properties": { "id": 5, "Ubicacion": "Acceso Sur-Lateral Este y Rawson", "Nombre": "Polideportivo B° La Gloria" }, "geometry": { "type": "Point", "coordinates": [ -68.831767231307069, -32.954428551096505 ] } },
+{ "type": "Feature", "properties": { "id": 6, "Ubicacion": "Biritos y Roque Saenz Peña", "Nombre": "Polideportivo Biritos" }, "geometry": { "type": "Point", "coordinates": [ -68.846225516507403, -32.910509463670138 ] } },
+{ "type": "Feature", "properties": { "id": 7, "Ubicacion": "Velez Sarfield y Cabo Blanco", "Nombre": "Polideportivo Los Peregrinos" }, "geometry": { "type": "Point", "coordinates": [ -68.830592291651953, -32.953059240532504 ] } },
+{ "type": "Feature", "properties": { "id": 8, "Ubicacion": "Juncal y Rio Gallegos", "Nombre": "Cedere" }, "geometry": { "type": "Point", "coordinates": [ -68.872119378906518, -32.909632672131671 ] } },
+{ "type": "Feature", "properties": { "id": 9, "Ubicacion": "Av Las Tipas y Lautaro", "Nombre": "Polideportivo Cecom" }, "geometry": { "type": "Point", "coordinates": [ -68.86696646514136, -32.905812415355875 ] } },
+{ "type": "Feature", "properties": { "id": 10, "Ubicacion": "Lorenso Soler y Plaza Huincul", "Nombre": "Polideportivo La Estanzuela" }, "geometry": { "type": "Point", "coordinates": [ -68.872388008272125, -32.942413707433531 ] } },
+{ "type": "Feature", "properties": { "id": 11, "Ubicacion": "Elena de Zizzias y J.A.Roca", "Nombre": "Polideportivo Filippini" }, "geometry": { "type": "Point", "coordinates": [ -68.840991009710663, -32.954776141940471 ] } },
+{ "type": "Feature", "properties": { "id": 12, "Ubicacion": "Chuquisaca entre Einstein y P. Iglesias", "Nombre": "Polideportivo Chuquisaca" }, "geometry": { "type": "Point", "coordinates": [ -68.872686764295551, -32.919702605877283 ] } },
+{ "type": "Feature", "properties": { "id": 13, "Ubicacion": "Los Aguaribay y Rivadavia", "Nombre": "Polideportivo Los Paraisos" }, "geometry": { "type": "Point", "coordinates": [ -68.86413330402857, -32.925457844572612 ] } },
+{ "type": "Feature", "properties": { "id": 14, "Ubicacion": "Rivadavia y Perito Moreno", "Nombre": "Plaza Godoy Cruz" }, "geometry": { "type": "Point", "coordinates": [ -68.843691111418096, -32.92488570925822 ] } },
+{ "type": "Feature", "properties": { "id": 15, "Ubicacion": "Guemes", "Nombre": "Polo Ambiental (Mun. Godoy Cruz)" }, "geometry": { "type": "Point", "coordinates": [ -68.851236583925413, -32.927913348324061 ] } }
+]
+}
