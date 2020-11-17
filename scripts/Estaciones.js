6d7f99aa9a1229eed67052792e2811cb5d16a86b
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2020-11-17 11:56:10 -0300

    Add files via upload

---
+++ scripts/Estaciones.js
@@ -0,0 +1,10 @@
+var Estaciones ={
+"type": "FeatureCollection",
+"name": "Estaciones",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Estación Godoy Cruz Centro" }, "geometry": { "type": "Point", "coordinates": [ -68.843286697555001, -32.925097988569107 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Estación Godoy Cruz Este" }, "geometry": { "type": "Point", "coordinates": [ -68.818525484155117, -32.943494248537036 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "Estación Godoy Cruz Oeste" }, "geometry": { "type": "Point", "coordinates": [ -68.877516039104393, -32.933509712259998 ] } }
+]
+}
