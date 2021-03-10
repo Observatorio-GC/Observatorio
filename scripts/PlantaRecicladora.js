c1c889f789927008fdbb06120868f507ed5d9751
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-03-10 10:40:53 -0300

    Add files via upload

---
+++ scripts/PlantaRecicladora.js
@@ -0,0 +1,8 @@
+var PlantaRecicladora ={
+"type": "FeatureCollection",
+"name": "Planta Recicladora",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1 }, "geometry": { "type": "Point", "coordinates": [ -68.826779017803645, -32.934715669274972 ] } }
+]
+}
