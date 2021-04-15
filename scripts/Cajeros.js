4a67aefc6636e321544206a58b63e22a9c07d047
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-04-15 09:53:59 -0300

    Add files via upload

---
+++ scripts/Cajeros.js
@@ -0,0 +1,23 @@
+var Cajeros ={
+"type": "FeatureCollection",
+"name": "Cajeros",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco Supervielle" }, "geometry": { "type": "Point", "coordinates": [ -68.864709684476864, -32.922877398268376 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco Supervielle" }, "geometry": { "type": "Point", "coordinates": [ -68.840813304260138, -32.939264721281859 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco Nacion" }, "geometry": { "type": "Point", "coordinates": [ -68.844006306996604, -32.924593365261771 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco Patagonia" }, "geometry": { "type": "Point", "coordinates": [ -68.8443471217753, -32.924538166570244 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco Supervielle" }, "geometry": { "type": "Point", "coordinates": [ -68.84454608390358, -32.92446429838769 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco Galicia" }, "geometry": { "type": "Point", "coordinates": [ -68.843601852091069, -32.924708392756877 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco Macro" }, "geometry": { "type": "Point", "coordinates": [ -68.843840891041097, -32.92612266348074 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco Supervielle" }, "geometry": { "type": "Point", "coordinates": [ -68.84388223831894, -32.926331982303864 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero Automatico", "Nombre_1": "Banco Nacion" }, "geometry": { "type": "Point", "coordinates": [ -68.820037054952991, -32.92779289859309 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco ICBC" }, "geometry": { "type": "Point", "coordinates": [ -68.806800689396482, -32.930542020625985 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero Automatico", "Nombre_1": "Link" }, "geometry": { "type": "Point", "coordinates": [ -68.859149177416725, -32.927000384749093 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banelco" }, "geometry": { "type": "Point", "coordinates": [ -68.856492662136446, -32.929349872380008 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero", "Nombre_1": "Banco San Juan" }, "geometry": { "type": "Point", "coordinates": [ -68.856117661392986, -32.928626488132409 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero Automatico", "Nombre_1": "Banco Supervielle" }, "geometry": { "type": "Point", "coordinates": [ -68.860204422809261, -32.90977634468527 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero Automatico", "Nombre_1": "Banco Galicia" }, "geometry": { "type": "Point", "coordinates": [ -68.858516985227212, -32.955279489535855 ] } },
+{ "type": "Feature", "properties": { "id": null, "Descrip_1": "Cajero Automatico", "Nombre_1": "Banco Patagonia" }, "geometry": { "type": "Point", "coordinates": [ -68.850080400655074, -32.930057253871396 ] } }
+]
+}
---
+++ scripts/Pagofacil.js
@@ -0,0 +1,15 @@
+var Pagofacil ={
+"type": "FeatureCollection",
+"name": "Pagofacil",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nombre_1": "Rapipago", "Descrip": "San Martin y Barraquero" }, "geometry": { "type": "Point", "coordinates": [ -68.843117387861483, -32.905570311564404 ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "Rapipago", "Descrip": "Pellegrini 1432" }, "geometry": { "type": "Point", "coordinates": [ -68.861115945037497, -32.908762008545992 ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "Rapipago", "Descrip": "Paso de Los Andes 1050" }, "geometry": { "type": "Point", "coordinates": [ -68.859817943899117, -32.914730772898558 ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "Rapipago", "Descrip": "Chuquisaca 1882" }, "geometry": { "type": "Point", "coordinates": [ -68.876806042245306, -32.912387276149239 ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "Rapipago", "Descrip": "Parana 1119" }, "geometry": { "type": "Point", "coordinates": [ -68.857229170279069, -32.920345070983821 ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "Rapipago", "Descrip": "Rivadavia 614" }, "geometry": { "type": "Point", "coordinates": [ -68.845514942099641, -32.924511180271111 ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "Pago Facil", "Descrip": "Palmares Open Mall" }, "geometry": { "type": "Point", "coordinates": [ -68.858106901397562, -32.955537737688374 ] } },
+{ "type": "Feature", "properties": { "Nombre_1": "Rapipago", "Descrip": "San Martin Sur 2575 Wallmart" }, "geometry": { "type": "Point", "coordinates": [ -68.857962898152905, -32.95182878540529 ] } }
+]
+}
