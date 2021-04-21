9e56115fbccf97cbf55798a5cc9ab84154202c2f
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-04-21 13:49:14 -0300

    Add files via upload

---
+++ scripts/JardinesMunicipales.js
@@ -0,0 +1,10 @@
+var JardinesMunicipales ={
+"type": "FeatureCollection",
+"name": "JardinesMunicipales",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "numero": "4", "nivel": "Jardin Maternal", "nombre": "Chispitas de Esperanza", "domicilio": "Pte Illia 981", "email": null, "telefono": "0261-156574886", "modalidad": "Estatal", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.870981668608593, -32.939269324287324 ] ] } },
+{ "type": "Feature", "properties": { "numero": "7", "nivel": "Jardin Maternal", "nombre": "Patitas Sueltas", "domicilio": "Lautaro 2284", "email": null, "telefono": "4288464", "modalidad": "Estatal", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.869742978150597, -32.905947983133807 ] ] } },
+{ "type": "Feature", "properties": { "numero": "3", "nivel": "Jardin Maternal", "nombre": "Picardias", "domicilio": "Manzana \"12\"", "email": null, "telefono": "0261-4397173", "modalidad": "Estatal", "id": 1 }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.869519010619129, -32.94742511066918 ] ] } }
+]
+}
