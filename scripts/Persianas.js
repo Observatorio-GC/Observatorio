38f3ea48950584e1a8295f6c5843e13006b3ed80
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-05-20 13:04:50 -0300

    Add files via upload

---
+++ scripts/Persianas.js
@@ -0,0 +1,11 @@
+var Persianas ={
+"type": "FeatureCollection",
+"name": "Persianas",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Ferreteria Suarez", "Fotos": "ferreteria.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.844552368262711, -32.927847777300471 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Imprenta Print Gráfica", "Fotos": "imprenta.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.844486366204862, -32.927740275222533 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "El Emporio", "Fotos": "elemporio.jpg"  }, "geometry": { "type": "Point", "coordinates": [ -68.844933572518386, -32.929012045262255 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Nombre": "Artistica" , "Fotos": "artistica.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.842946965776903, -32.923211699343021 ] } }
+]
+}
