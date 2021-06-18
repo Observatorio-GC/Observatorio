bf9745b824ea4f4e37a8643bf8d1a71de7ce0dd2
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-06-18 13:10:08 -0300

    Add files via upload

---
+++ scripts/Puntoswifi.js
@@ -0,0 +1,19 @@
+var Puntoswifi ={
+"type": "FeatureCollection",
+"name": "Puntoswifi",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Zona": "Zona 12", "x": -32.940748022559703, "y": -68.830522245918004, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.830522245918004, -32.940748022559703 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 11", "x": -32.938464293362699, "y": -68.8412786117824, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.8412786117824, -32.938464293362699 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 10", "x": -32.945533190609801, "y": -68.843891454120495, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.843891454120495, -32.945533190609801 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 9", "x": -32.929337428230703, "y": -68.839063687749501, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.839063687749501, -32.929337428230703 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 8", "x": -32.939526471454499, "y": -68.863297572997894, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.863297572997894, -32.939526471454499 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 7", "x": -32.930421980324603, "y": -68.839465070710105, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.839465070710105, -32.930421980324603 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 6", "x": -32.9214402441376, "y": -68.853696693809695, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.853696693809695, -32.9214402441376 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 5", "x": -32.9301687292228, "y": -68.864155843087104, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.864155843087104, -32.9301687292228 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 4", "x": -32.905382028252397, "y": -68.843184599507794, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.843184599507794, -32.905382028252397 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 3", "x": -32.9039964029133, "y": -68.842956005159706, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.842956005159706, -32.9039964029133 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 2", "x": -32.905425178965899, "y": -68.854074597887205, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.854074597887205, -32.905425178965899 ] } },
+{ "type": "Feature", "properties": { "Zona": "Zona 1", "x": -32.914631777476899, "y": -68.863995252554602, "field_4": "Puntos WiFi gratis" }, "geometry": { "type": "Point", "coordinates": [ -68.863995252554602, -32.914631777476899 ] } }
+]
+}
