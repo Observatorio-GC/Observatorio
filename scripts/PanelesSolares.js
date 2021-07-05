c2d3529bb61ccbfde1d00f8726e8105bc05ce0bf
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-07-05 13:15:32 -0300

    Add files via upload

---
+++ scripts/PanelesSolares.js
@@ -0,0 +1,15 @@
+var PanelesSolares ={
+"type": "FeatureCollection",
+"name": "paneles solares",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Ubicacion": "Parque Margarita Malharro de Torres" }, "geometry": { "type": "Point", "coordinates": [ -68.829133085363054, -32.931959553941567 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Ubicacion": "Polideportivo N°1 Biritos" }, "geometry": { "type": "Point", "coordinates": [ -68.846294053021595, -32.910525434113111 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Ubicacion": "Plaza Solar" }, "geometry": { "type": "Point", "coordinates": [ -68.856408099006202, -32.950088776219587 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Ubicacion": "Espacio Verde Luis Menotti Pescarmona" }, "geometry": { "type": "Point", "coordinates": [ -68.841740866268339, -32.923891752219923 ] } },
+{ "type": "Feature", "properties": { "id": 5, "Ubicacion": "H.C.D (Casa Municipal)" }, "geometry": { "type": "Point", "coordinates": [ -68.843362642175975, -32.924963989226001 ] } },
+{ "type": "Feature", "properties": { "id": 6, "Ubicacion": "Parque Mitre" }, "geometry": { "type": "Point", "coordinates": [ -68.840454981186753, -32.921338551241604 ] } },
+{ "type": "Feature", "properties": { "id": 7, "Ubicacion": "Polo Ambiental" }, "geometry": { "type": "Point", "coordinates": [ -68.851196701947316, -32.927996939877907 ] } },
+{ "type": "Feature", "properties": { "id": 8, "Ubicacion": "Estacion Alsina" }, "geometry": { "type": "Point", "coordinates": [ -68.826059175379044, -32.93246501296678 ] } }
+]
+}
