da872f93fae22681d6c525f19c21516bb22f9e29
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-10-29 09:11:33 -0300

    Add files via upload

--- scripts/Estaciones.js
+++ scripts/Estaciones.js
@@ -3,8 +3,8 @@ var Estaciones ={
 "name": "Estaciones",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": 1, "Nombre": "Estación Godoy Cruz Centro" }, "geometry": { "type": "Point", "coordinates": [ -68.843286697555001, -32.925097988569107 ] } },
-{ "type": "Feature", "properties": { "id": 2, "Nombre": "Estación Godoy Cruz Este" }, "geometry": { "type": "Point", "coordinates": [ -68.818525484155117, -32.943494248537036 ] } },
-{ "type": "Feature", "properties": { "id": 3, "Nombre": "Estación Godoy Cruz Oeste" }, "geometry": { "type": "Point", "coordinates": [ -68.877516039104393, -32.933509712259998 ] } }
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Estación Godoy Cruz Centro", "Direccion": "Casa Municipal" }, "geometry": { "type": "Point", "coordinates": [ -68.843286697555001, -32.925097988569107 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Estación Godoy Cruz Este", "Direccion": "Casa del Futuro" }, "geometry": { "type": "Point", "coordinates": [ -68.818525484155117, -32.943494248537036 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "Estación Godoy Cruz Oeste", "Direccion": "CCT Dolores P. de Huisi"}, "geometry": { "type": "Point", "coordinates": [ -68.877516039104393, -32.933509712259998 ] } }
 ]
 }
