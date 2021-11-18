f4a0abd654e129db2db5d0477ab81d17208a4396
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-11-18 12:04:20 -0300

    Add files via upload

---
+++ scripts/ParadasMetrotranvia.js
@@ -0,0 +1,15 @@
+var ParadasMetro ={
+"type": "FeatureCollection",
+"name": "Paradas Metrotranvia",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "25 de Mayo" }, "geometry": { "type": "Point", "coordinates": [ -68.853005594310986, -32.904386167737151 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Pellegrini" }, "geometry": { "type": "Point", "coordinates": [ -68.851946184044309, -32.908650718945459 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "San Martin" }, "geometry": { "type": "Point", "coordinates": [ -68.846578505359886, -32.915929392172437 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Nombre": "Mitre" }, "geometry": { "type": "Point", "coordinates": [ -68.839705051549757, -32.921552225847762 ] } },
+{ "type": "Feature", "properties": { "id": 5, "Nombre": "Progreso" }, "geometry": { "type": "Point", "coordinates": [ -68.833249711658212, -32.926883034718024 ] } },
+{ "type": "Feature", "properties": { "id": 6, "Nombre": "Independencia" }, "geometry": { "type": "Point", "coordinates": [ -68.826040071656791, -32.932787336904333 ] } },
+{ "type": "Feature", "properties": { "id": 7, "Nombre": "Parque TIC" }, "geometry": { "type": "Point", "coordinates": [ -68.819242895385884, -32.938351010403714 ] } },
+{ "type": "Feature", "properties": { "id": 8, "Nombre": "9 de Julio" }, "geometry": { "type": "Point", "coordinates": [ -68.815152159209532, -32.941701138033196 ] } }
+]
+}
