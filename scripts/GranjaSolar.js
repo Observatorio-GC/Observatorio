1146ce109043a54a914fdfb11d5671defff64bc8
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2021-11-24 09:49:43 -0300

    Add files via upload

---
+++ scripts/GranjaSolar.js
@@ -0,0 +1,8 @@
+var GranjaSolar ={
+"type": "FeatureCollection",
+"name": "GranjaSolar",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre_1": "Granja Solar (Proyectada)" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -68.877445836289084, -32.934300560651749 ], [ -68.876164068013736, -32.933735217990382 ], [ -68.875701071868647, -32.934865903313124 ], [ -68.877168038602036, -32.93546536147992 ], [ -68.877445836289084, -32.934300560651749 ] ] ] ] } }
+]
+}
