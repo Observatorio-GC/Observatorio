57b904d5f02f98c75e06ebcd9eb772c851c1bd17
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-01-10 13:46:51 -0300

    Add files via upload

---
+++ scripts/Incubadora.js
@@ -0,0 +1,8 @@
+var Incubadora ={
+"type": "FeatureCollection",
+"name": "Incubadora",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre_1": "Incubadora de Empresas y Escuelas de oficios" }, "geometry": { "type": "Point", "coordinates": [ -68.847518023612182, -32.933359136576939 ] } }
+]
+}
