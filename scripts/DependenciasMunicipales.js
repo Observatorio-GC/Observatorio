4544214cae32583df90378ebb7d80c4a07c9bad0
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-02-07 10:30:53 -0300

    Add files via upload

--- scripts/DependenciasMunicipales.js
+++ scripts/DependenciasMunicipales.js
@@ -1,4 +1,4 @@
-var DependenciasMunicipales={
+var DependenciasMunicipales ={
 "type": "FeatureCollection",
 "name": "DependenciasMunicipales",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
@@ -11,7 +11,7 @@ var DependenciasMunicipales={
 { "type": "Feature", "properties": { "ID": "6", "Nombre_1": "CAU II", "Direccion": "Paso de los Andes esq. Laprida" }, "geometry": { "type": "Point", "coordinates": [ -68.859673237182719, -32.909873260111382 ] } },
 { "type": "Feature", "properties": { "ID": "7", "Nombre_1": "CAU III", "Direccion": "Sarmiento 585 (Paseo Stare)" }, "geometry": { "type": "Point", "coordinates": [ -68.840297517226787, -32.938669034643574 ] } },
 { "type": "Feature", "properties": { "ID": "8", "Nombre_1": "GALPONES PESCARMONA", "Direccion": "Balcarce esq. Rivadavia" }, "geometry": { "type": "Point", "coordinates": [ -68.842018547063731, -32.92408172115524 ] } },
-{ "type": "Feature", "properties": { "ID": "9", "Nombre_1": "CRISTOFORO COLOMBO", "Direccion": "Antonio Tomba 246" }, "geometry": { "type": "Point", "coordinates": [ -68.841979565575357, -32.921909041183874 ] } },
+{ "type": "Feature", "properties": { "ID": "9", "Nombre_1": "CRISTOFORO COLOMBO", "Direccion": "Antonio Tomba 246" }, "geometry": { "type": "Point", "coordinates": [ -68.84284357781226, -32.92210210424976 ] } },
 { "type": "Feature", "properties": { "ID": "10", "Nombre_1": "CINE TEATRO PLAZA", "Direccion": "Colon 27" }, "geometry": { "type": "Point", "coordinates": [ -68.845222820783789, -32.924899734369923 ] } },
 { "type": "Feature", "properties": { "ID": "11", "Nombre_1": "EX COMISARIA 7", "Direccion": "Lavalle 88" }, "geometry": { "type": "Point", "coordinates": [ -68.845086385796918, -32.925845348293038 ] } },
 { "type": "Feature", "properties": { "ID": "12", "Nombre_1": "CASA DEL FUTURO", "Direccion": "Sarmiento 2291" }, "geometry": { "type": "Point", "coordinates": [ -68.817781842314119, -32.943654704232529 ] } },
