70e502e51c52cfcdf15a5ef27f49999099b6a54d
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-03-15 12:45:43 -0300

    Update DependenciasMunicipales.js

--- scripts/DependenciasMunicipales.js
+++ scripts/DependenciasMunicipales.js
@@ -5,8 +5,8 @@ var DependenciasMunicipales ={
 "features": [
 { "type": "Feature", "properties": { "ID": "1", "Nombre_1": "CASA MUNICIPAL", "Direccion": "Rivadavia 448" }, "geometry": { "type": "Point", "coordinates": [ -68.843189939118346, -32.925125504739498 ] } },
 { "type": "Feature", "properties": { "ID": "2", "Nombre_1": "CORRALON MUNICIPAL", "Direccion": "Juncal 399" }, "geometry": { "type": "Point", "coordinates": [ -68.865319697938119, -32.923347138359098 ] } },
-{ "type": "Feature", "properties": { "ID": "3", "Nombre_1": "SEDE GUEMES", "Direccion": "Guemes esq. Ciclovia" }, "geometry": { "type": "Point", "coordinates": [ -68.851430613889278, -32.928045742832175 ] } },
-{ "type": "Feature", "properties": { "ID": "4", "Nombre_1": "SEDE LIBERTAD", "Direccion": "Joaquin V. Gonzalez 450" }, "geometry": { "type": "Point", "coordinates": [ -68.857921022327645, -32.929347957935548 ] } },
+{ "type": "Feature", "properties": { "ID": "3", "Nombre_1": "SEDE GÜEMES", "Direccion": "Güemes esq. Ciclovia" }, "geometry": { "type": "Point", "coordinates": [ -68.851430613889278, -32.928045742832175 ] } },
+{ "type": "Feature", "properties": { "ID": "4", "Nombre_1": "SEDE LIBERTAD", "Direccion": "Joaquín V. González 450" }, "geometry": { "type": "Point", "coordinates": [ -68.857921022327645, -32.929347957935548 ] } },
 { "type": "Feature", "properties": { "ID": "5", "Nombre_1": "CAU I", "Direccion": "Perito Moreno 72" }, "geometry": { "type": "Point", "coordinates": [ -68.84344331843856, -32.92563103186724 ] } },
 { "type": "Feature", "properties": { "ID": "6", "Nombre_1": "CAU II", "Direccion": "Paso de los Andes esq. Laprida" }, "geometry": { "type": "Point", "coordinates": [ -68.859673237182719, -32.909873260111382 ] } },
 { "type": "Feature", "properties": { "ID": "7", "Nombre_1": "CAU III", "Direccion": "Sarmiento 585 (Paseo Stare)" }, "geometry": { "type": "Point", "coordinates": [ -68.840297517226787, -32.938669034643574 ] } },
@@ -16,9 +16,9 @@ var DependenciasMunicipales ={
 { "type": "Feature", "properties": { "ID": "11", "Nombre_1": "EX COMISARIA 7", "Direccion": "Lavalle 88" }, "geometry": { "type": "Point", "coordinates": [ -68.845086385796918, -32.925845348293038 ] } },
 { "type": "Feature", "properties": { "ID": "12", "Nombre_1": "CASA DEL FUTURO", "Direccion": "Sarmiento 2291" }, "geometry": { "type": "Point", "coordinates": [ -68.817781842314119, -32.943654704232529 ] } },
 { "type": "Feature", "properties": { "ID": "13", "Nombre_1": "MEDIATECA MUNICPAL", "Direccion": "Antonio Tomba 54" }, "geometry": { "type": "Point", "coordinates": [ -68.843456961900415, -32.92412425800525 ] } },
-{ "type": "Feature", "properties": { "ID": "14", "Nombre_1": "BODEGA ARIZU", "Direccion": "San Martin entre Minuzzi y Lisandro de la Torre" }, "geometry": { "type": "Point", "coordinates": [ -68.843870164674868, -32.908618211886939 ] } },
+{ "type": "Feature", "properties": { "ID": "14", "Nombre_1": "BODEGA ARIZU", "Direccion": "Av. San Martín entre Minuzzi y Lisandro de la Torre" }, "geometry": { "type": "Point", "coordinates": [ -68.843870164674868, -32.908618211886939 ] } },
 { "type": "Feature", "properties": { "ID": null, "Nombre_1": "CEMENTERIO", "Direccion": "Cervantes y Alsina" }, "geometry": { "type": "Point", "coordinates": [ -68.839503856105438, -32.933435768802639 ] } },
 { "type": "Feature", "properties": { "ID": null, "Nombre_1": "CASA DE LOS ABUELOS", "Direccion": "Derqui y Marmol" }, "geometry": { "type": "Point", "coordinates": [ -68.85714856266037, -32.916746261269061 ] } },
-{ "type": "Feature", "properties": { "ID": null, "Nombre_1": "JUZGADO DE TRANSITO", "Direccion": "J.V.Gonzalez y Ing.Cipolletti" }, "geometry": { "type": "Point", "coordinates": [ -68.858653118261117, -32.932634056201437 ] } }
+{ "type": "Feature", "properties": { "ID": null, "Nombre_1": "JUZGADO DE TRANSITO", "Direccion": "J.V.González y Ing.Cipolletti" }, "geometry": { "type": "Point", "coordinates": [ -68.858653118261117, -32.932634056201437 ] } }
 ]
 }
