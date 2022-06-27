07a98d403ab1f78abe6d6e00c769d010fdd1d758
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-06-27 10:31:43 -0300

    Add files via upload

---
+++ scripts/Ciclovias Proyectadas (Municipalidad).js
@@ -0,0 +1,8 @@
+var CicloviasProyectadasMuni ={
+"type": "FeatureCollection",
+"name": "Ciclovias Proyectadas (Municipalidad)",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Distancia": "BELGRANO (2,16 Km)" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -68.844346215302068, -32.922489299496917 ], [ -68.842666692001103, -32.915869107649968 ], [ -68.842496305579274, -32.914683959320335 ], [ -68.842350260074838, -32.912293180778363 ], [ -68.843177851266617, -32.912313615056576 ], [ -68.842715373835915, -32.907828177877654 ], [ -68.842155532735589, -32.90406800077961 ] ] ] } }
+]
+}
