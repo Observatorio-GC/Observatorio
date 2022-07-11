99f6f29f5dd94bfa5aae6f6f6c5e4df7289a03b9
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-07-11 10:14:28 -0300

    Add files via upload

--- scripts/Puestos.js
+++ scripts/Puestos.js
@@ -7,6 +7,7 @@ var Puestos ={
 { "type": "Feature", "properties": { "Nombre": "El Higueral" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.954919708359739, -32.934400759751753 ] ] } },
 { "type": "Feature", "properties": { "Nombre": "Estancia Punta del Agua" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.970219712439004, -32.932478934885445 ] ] } },
 { "type": "Feature", "properties": { "Nombre": "Las Trancas" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.998489221623501, -32.925314937757243 ] ] } },
-{ "type": "Feature", "properties": { "Nombre": "Garelli" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.950133251707911, -32.935316695065659 ] ] } }
+{ "type": "Feature", "properties": { "Nombre": "Garelli" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.950133251707911, -32.935316695065659 ] ] } },
+{ "type": "Feature", "properties": { "Nombre": "Victor Rico" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ -68.951564303667865, -32.92673967503103 ] ] } }
 ]
 }
