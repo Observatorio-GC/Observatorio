ae06e0ca3d322c21a24f5129eddbbcf9b4a27ef6
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2022-07-28 10:34:23 -0300

    Add files via upload

---
+++ scripts/Diques.js
@@ -0,0 +1,10 @@
+var Diques ={
+"type": "FeatureCollection",
+"name": "Diques",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Dique Tajamar", "Categor.": null, "AÑO": null, "UBICACION": "100 m al Sur de calle Montecaseros", "DISTRITO": "Centro", "USO": null, "PROPIETARI": null, "Fotos": "diqueTajamar.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.837772551196039, -32.919870461666704 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Dique El Pilar", "Categor.": null, "AÑO": "Construcción año 1940", "UBICACION": "100 m al Norte de calle Álvarez Thomas", "DISTRITO": "Las Tortugas", "USO": null, "PROPIETARI": null, "Fotos": "diquePilar.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.838993965600068, -32.929356097881573 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "Planta potabilizadora", "Categor.": null, "AÑO": "Construcción año 1930 ca. / Remodelación década de 1990", "UBICACION": "Perito Moreno y Curie", "DISTRITO": "Gobernador Benegas", "USO": null, "PROPIETARI": null, "Fotos": "plantaPota.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.849761788901432, -32.950188398198897 ] } }
+]
+}
