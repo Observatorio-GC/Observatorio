34d75c13bee0cc137a4b1cc20b1fde050fb5cc2d
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2023-02-27 11:50:51 -0300

    Update GranjaSolar.js

--- scripts/GranjaSolar.js
+++ scripts/GranjaSolar.js
@@ -3,6 +3,6 @@ var GranjaSolar ={
 "name": "GranjaSolar",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": 1, "Nombre_1": "Granja Solar (Proyectada)", "Descrip": "Producción estimada: 630 Kw - Contará con 1144 paneles solares" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -68.877445836289084, -32.934300560651749 ], [ -68.876164068013736, -32.933735217990382 ], [ -68.875701071868647, -32.934865903313124 ], [ -68.877168038602036, -32.93546536147992 ], [ -68.877445836289084, -32.934300560651749 ] ] ] ] } }
+{ "type": "Feature", "properties": { "id": 1, "Nombre_1": "Granja Solar (Proyectada)", "Descrip": "Producción estimada: 630 Kw - Contará con 1144 paneles solares. Abastecerá el consumo de 20.000 luminarias de alumbrado público." }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -68.877445836289084, -32.934300560651749 ], [ -68.876164068013736, -32.933735217990382 ], [ -68.875701071868647, -32.934865903313124 ], [ -68.877168038602036, -32.93546536147992 ], [ -68.877445836289084, -32.934300560651749 ] ] ] ] } }
 ]
 }
