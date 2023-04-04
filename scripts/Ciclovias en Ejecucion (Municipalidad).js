99ed59b289d5d3f04daf09d3eb0db1c1b1805d5f
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2023-04-04 09:55:07 -0300

    Delete Ciclovias en Ejecucion (Municipalidad).js

--- scripts/Ciclovias en Ejecucion (Municipalidad).js
+++
@@ -1,8 +0,0 @@
-var CicloviasenEjecucionMuni ={
-"type": "FeatureCollection",
-"name": "Ciclovias en Ejecucion (Municipalidad)",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Distancia": "METROTRANVIA DESDE PARQUE MITRE A 9 DE JULIO (3,2 km)", "Id": "1", "TIPO_2": "Ciclovia" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -68.839511688502867, -32.921846459252656 ], [ -68.838036539939949, -32.923041826200823 ], [ -68.835523678643398, -32.925179241421837 ], [ -68.832777731102055, -32.927462667471502 ], [ -68.826238544398947, -32.932961944654885 ], [ -68.822751829613878, -32.935684657111267 ], [ -68.818492888138437, -32.939131105832445 ] ] ] } }
-]
-}
