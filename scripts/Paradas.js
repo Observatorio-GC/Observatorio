daa7cf079e1227c8352c51ae94196ed0f4440fc5
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2023-06-02 11:23:55 -0300

    Add files via upload

---
+++ scripts/Paradas.js
@@ -0,0 +1,15 @@
+var Paradas ={
+"type": "FeatureCollection",
+"name": "Paradas",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Chacabuco" }, "geometry": { "type": "Point", "coordinates": [ -68.850763408532629, -32.917048327959563 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Rivadavia" }, "geometry": { "type": "Point", "coordinates": [ -68.851894831354159, -32.923647952461664 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "San Vicente" }, "geometry": { "type": "Point", "coordinates": [ -68.851294949455493, -32.93130427053503 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Nombre": "San Martin Sur" }, "geometry": { "type": "Point", "coordinates": [ -68.85037899054376, -32.939623491149462 ] } },
+{ "type": "Feature", "properties": { "id": 5, "Nombre": "Trapiche" }, "geometry": { "type": "Point", "coordinates": [ -68.85113833471928, -32.944606841717352 ] } },
+{ "type": "Feature", "properties": { "id": 6, "Nombre": "Benegas" }, "geometry": { "type": "Point", "coordinates": [ -68.853393586920618, -32.951893371090549 ] } },
+{ "type": "Feature", "properties": { "id": 7, "Nombre": "Carrodilla" }, "geometry": { "type": "Point", "coordinates": [ -68.85276712797581, -32.958492785183125 ] } },
+{ "type": "Feature", "properties": { "id": 8, "Nombre": "Pellegrini" }, "geometry": { "type": "Point", "coordinates": [ -68.852100447519163, -32.908477662779568 ] } }
+]
+}
