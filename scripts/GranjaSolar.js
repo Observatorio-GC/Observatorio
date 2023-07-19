f04ec50ff945d8b5474541e3ce57e70693a41001
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2023-07-19 11:03:21 -0300

    Add files via upload

--- scripts/GranjaSolar.js
+++ scripts/GranjaSolar.js
@@ -3,6 +3,6 @@ var GranjaSolar ={
 "name": "GranjaSolar",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": 1, "Nombre_1": "Parque Solar", "Descrip": "Producción estimada: 630 Kw - Contará con 1144 paneles solares. Abastecerá el consumo de 20.000 luminarias de alumbrado público." }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -68.877445836289084, -32.934300560651749 ], [ -68.876164068013736, -32.933735217990382 ], [ -68.875701071868647, -32.934865903313124 ], [ -68.877168038602036, -32.93546536147992 ], [ -68.877445836289084, -32.934300560651749 ] ] ] ] } }
+{ "type": "Feature", "properties": { "id": 1, "Nombre_1": "Parque Solar", "Descrip": "Producción estimada: 630 Kw - Contará con 1144 paneles solares. Abastecerá el consumo de 20.000 luminarias de alumbrado público." }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -68.877378112145209, -32.934426931055995 ], [ -68.877294509630801, -32.934329755722437 ], [ -68.876621643424869, -32.933958373457727 ], [ -68.876105274722292, -32.933954920385638 ], [ -68.875920041135529, -32.934181287591549 ], [ -68.875710058364433, -32.934640065305153 ], [ -68.876955162168485, -32.934988479411743 ], [ -68.877276398187576, -32.934812262075141 ], [ -68.877378112145209, -32.934426931055995 ] ] ] ] } }
 ]
 }
