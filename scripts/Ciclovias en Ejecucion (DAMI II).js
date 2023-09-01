52cb4bb030eab6f87105fcbf1332ea54bf5f26b4
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2023-09-01 10:25:49 -0300

    Add files via upload

--- scripts/Ciclovias en Ejecucion (DAMI II).js
+++ scripts/Ciclovias en Ejecucion (DAMI II).js
@@ -3,7 +3,6 @@ var CicloviasenEjecucionDami ={
 "name": "Ciclovias en Ejecucion (DAMI II)",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "Distancia": "Frente a Cerveceria (0,16 Km)", "Id": "1", "TIPO_2": "Ciclovia: CERVANTES SUR" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -68.846905054244218, -32.953033586624223 ], [ -68.846034866446999, -32.951787660700603 ] ] ] } },
-{ "type": "Feature", "properties": { "Distancia": "Puente Olive-Primitivo de la Reta (0,33 Km)", "Id": "2", "TIPO_2": "Ciclovia: CARRIL SARMIENTO " }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -68.841103784455683, -32.938013847461917 ], [ -68.840928367075051, -32.938116678340215 ], [ -68.840724721610172, -32.938148939007917 ], [ -68.840569467146864, -32.938320323805087 ], [ -68.840388384877471, -32.938344876206827 ], [ -68.8397863279191, -32.938501687105727 ], [ -68.837834832950605, -32.938989541457417 ] ] ] } }
+{ "type": "Feature", "properties": { "Distancia": "Frente a Cerveceria (0,16 Km)", "Id": "1", "TIPO_2": "Ciclovia: CERVANTES SUR" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ -68.846905054244218, -32.953033586624223 ], [ -68.846034866446999, -32.951787660700603 ] ] ] } }
 ]
 }
