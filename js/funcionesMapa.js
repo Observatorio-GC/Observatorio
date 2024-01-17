c41ec4660a5e6155b94e7def2079b8c69b114ee0
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2024-01-17 12:34:12 -0300

    Add files via upload

--- js/funcionesMapa.js
+++ js/funcionesMapa.js
@@ -5,7 +5,7 @@ map.on('overlayremove', onOverlayRemove);
 L.control.scale().addTo(map);
 L.control.polylineMeasure().addTo(map);
 L.streetView({ position: 'topleft' }).addTo(map);
-L.streetView1({ position: 'topleft' }).addTo(map);
+L.streetView1({ position: 'topleft' }).addTo(map); 
 
 
 /* IMPRESIÓN DE PANTALLA */
@@ -27,7 +27,7 @@ L.streetView1({ position: 'topleft' }).addTo(map);
     
 
 /* CARGA ARCHIVOS */
-(function (window) {
+/* (function (window) {
     
     function initMap() {
     var control;
@@ -62,8 +62,7 @@ control.loader.on('data:loaded', function (e) {
 
 window.addEventListener('load', function () {
  initMap();
- });}(window));
-
+ });}(window)); */
 
 /* COORDENADAS CLICK MOUSE */
   var c = new L.Control.Coordinates();
@@ -74,6 +73,44 @@ c.addTo(map);
 
 
 
+/*BUSCARDOR */      
+// create the geocoding control and add it to the map
+var searchControl = L.esri.Geocoding.geosearch({
+    position: 'topleft',
+    placeholder: "Ingresar dirección",
+    useMapBounds: false,
+    providers: [
+      L.esri.Geocoding.arcgisOnlineProvider({
+        // API Key to be passed to the ArcGIS Online Geocoding Service
+        apikey: 'AAPK2a3d19d93e504102bbcf74c44d0f102bQ2cZ2PdwTUD_Y_EyWj1bDTnWECSMJinF58T6J2OczD7lzOLAJJ8IOJ5R1I1ySl9x',
+        nearby: {
+            lat: -32.9337,
+            lng: -68.8978
+          }
+        })
+      ]
+    }).addTo(map);
+
+  // create an empty layer group to store the results and add it to the map
+  const results = L.layerGroup().addTo(map);
+
+  // listen for the results event and add every result to the map
+  searchControl.on("results", (data) => {
+    results.clearLayers();
+
+    for (let i = data.results.length - 1; i >= 0; i--) {
+      const marker = L.marker(data.results[i].latlng);
+
+      const lngLatString = `${Math.round(data.results[i].latlng.lng * 100000) / 100000}, ${
+        Math.round(data.results[i].latlng.lat * 100000) / 100000
+      }`;
+      marker.bindPopup(`<p>${data.results[i].properties.LongLabel}</p>Coordenadas: <b>${lngLatString}</b>`);
+
+      results.addLayer(marker);
+
+      marker.openPopup();
+    }
+  });
 
 /* TEXTOS CICLOVIAS */
 function onOverlayAdd(e) {
