c3893dd9d4a0b776dd5adfcf52bd15763f8b882f
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2024-04-30 13:00:28 -0300

    Add files via upload

---
+++ scripts/Bicitran.js
@@ -0,0 +1,24 @@
+var Bicitran ={
+"type": "FeatureCollection",
+"name": "Bicitran",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 13, "Nombre": "13- Mariano Moreno" }, "geometry": { "type": "Point", "coordinates": [ -68.853130877139563, -32.902837669063594 ] } },
+{ "type": "Feature", "properties": { "id": 16, "Nombre": "16- Pellegrini" }, "geometry": { "type": "Point", "coordinates": [ -68.852252410846518, -32.908502429453463 ] } },
+{ "type": "Feature", "properties": { "id": 15, "Nombre": "15- Arizu" }, "geometry": { "type": "Point", "coordinates": [ -68.844048646209956, -32.908858631437354 ] } },
+{ "type": "Feature", "properties": { "id": 18, "Nombre": "18- Anzorena" }, "geometry": { "type": "Point", "coordinates": [ -68.846309593492606, -32.916295514643956 ] } },
+{ "type": "Feature", "properties": { "id": 17, "Nombre": "17- Chacabuco" }, "geometry": { "type": "Point", "coordinates": [ -68.850803012651383, -32.916113843167977 ] } },
+{ "type": "Feature", "properties": { "id": 54, "Nombre": "54- Villa Hipodromo" }, "geometry": { "type": "Point", "coordinates": [ -68.860333731234675, -32.91036948566633 ] } },
+{ "type": "Feature", "properties": { "id": 79, "Nombre": "79- Pompeya" }, "geometry": { "type": "Point", "coordinates": [ -68.859287260042706, -32.916481966560475 ] } },
+{ "type": "Feature", "properties": { "id": 21, "Nombre": "21- Parque Alfosin" }, "geometry": { "type": "Point", "coordinates": [ -68.85158181502139, -32.921525580918008 ] } },
+{ "type": "Feature", "properties": { "id": 20, "Nombre": "20- Plaza Godoy Cruz" }, "geometry": { "type": "Point", "coordinates": [ -68.843799486402276, -32.925371442489904 ] } },
+{ "type": "Feature", "properties": { "id": 19, "Nombre": "19- Parque Mitre" }, "geometry": { "type": "Point", "coordinates": [ -68.840063513058581, -32.921444311685541 ] } },
+{ "type": "Feature", "properties": { "id": 22, "Nombre": "22- San Vicente" }, "geometry": { "type": "Point", "coordinates": [ -68.851273568745029, -32.931708144649313 ] } },
+{ "type": "Feature", "properties": { "id": 23, "Nombre": "23- Puente ciclovía" }, "geometry": { "type": "Point", "coordinates": [ -68.850239911486071, -32.940030911932709 ] } },
+{ "type": "Feature", "properties": { "id": 58, "Nombre": "58- Tiburcio Benegas" }, "geometry": { "type": "Point", "coordinates": [ -68.858204482707293, -32.937938643130501 ] } },
+{ "type": "Feature", "properties": { "id": 24, "Nombre": "24- Parque Benegas" }, "geometry": { "type": "Point", "coordinates": [ -68.853094570996006, -32.952381577978343 ] } },
+{ "type": "Feature", "properties": { "id": 60, "Nombre": "60- Cervantes" }, "geometry": { "type": "Point", "coordinates": [ -68.843071227878823, -32.947808077912931 ] } },
+{ "type": "Feature", "properties": { "id": 61, "Nombre": "61- Puente Olive" }, "geometry": { "type": "Point", "coordinates": [ -68.841248801857532, -32.938889789168073 ] } },
+{ "type": "Feature", "properties": { "id": 62, "Nombre": "62- Acceso Sur" }, "geometry": { "type": "Point", "coordinates": [ -68.829613750727759, -32.940694070753061 ] } }
+]
+}
