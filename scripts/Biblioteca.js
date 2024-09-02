8c32e1e7b1afb8a61fa96d2bad99c48cefeff02b
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2024-09-02 12:46:25 -0300

    Add files via upload

---
+++ scripts/Biblioteca.js
@@ -0,0 +1,10 @@
+var Biblioteca ={
+"type": "FeatureCollection",
+"name": "Biblioteca",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Nombre": "Biblioteca Popular Pbro. Pedro Arce" }, "geometry": { "type": "Point", "coordinates": [ -68.825748557404694, -32.939735040130579 ] } },
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Manuel Belgrano" }, "geometry": { "type": "Point", "coordinates": [ -68.843498086461182, -32.924100853036521 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Casa de la Memoria y la cultura popular" }, "geometry": { "type": "Point", "coordinates": [ -68.86784891255499, -32.92234016442557 ] } }
+]
+}
---
+++ scripts/Universidades.js
@@ -0,0 +1,11 @@
+var Universidades ={
+"type": "FeatureCollection",
+"name": "Universidades",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Universidad Siglo 21 - CAU Palmares" }, "geometry": { "type": "Point", "coordinates": [ -68.857609770709686, -32.954412888752877 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Universidad Católica Argentina" }, "geometry": { "type": "Point", "coordinates": [ -68.852626846998973, -32.933689548085731 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Nombre": "Universidad Champagnat" }, "geometry": { "type": "Point", "coordinates": [ -68.842562671424773, -32.916867034725364 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Nombre": "Instituto Universitario de Seguridad Pública" }, "geometry": { "type": "Point", "coordinates": [ -68.867347149778553, -32.917903665889533 ] } }
+]
+}
