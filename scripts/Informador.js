bf0083c3a9ba13f251205f68eb5ce4edca511cca
Observatorio-GC <67073781+Observatorio-GC@users.noreply.github.com>
2025-02-14 10:04:16 -0300

    Add files via upload

--- scripts/Informador.js
+++ scripts/Informador.js
@@ -3,6 +3,7 @@ var Informador ={
 "name": "Informador",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "id": 1, "Nombre": "Informador Turistico", "Tel": "261-6394428", "Ubicacion": "Palmares Open Mall", "Fotos": "palmares.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.857760457944664, -32.95484927127626 ] } }
+{ "type": "Feature", "properties": { "id": 1, "Nombre": "Informador turístico", "Tel": "261-6394428", "Ubicacion": "Palmares Open Mall", "Fotos": "palmares.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.857760457944664, -32.95484927127626 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Nombre": "Informador turístico", "Tel": "", "Ubicacion": "Plaza Godoy Cruz", "Fotos": "plazaGC.jpg" }, "geometry": { "type": "Point", "coordinates": [ -68.844975410524711, -32.925551805296401 ] } }
 ]
 }
